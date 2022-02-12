c224acafe43190cdacb212aa0373f56f8067d03e
Vladislav <98847589+PythonCodeExamples@users.noreply.github.com>
2022-02-12 06:30:13 -0900

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 sudo apt install mpv
+sudo apt install curl
 pip3 install -r requirements.txt
-sudo pip3 install -t /usr/bin/ --upgrade youtube-dl
 chmod +x app/main.py start_app.sh
-sudo ln -s $PWD/start_app.sh /usr/bin/pyyt
+
+sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+sudo chmod a+rx /usr/local/bin/yt-dlp
+
+sudo ln -f -s /usr/local/bin/yt-dlp /usr/bin/youtube-dl
+sudo ln -f -s $PWD/start_app.sh /usr/bin/pyyt
